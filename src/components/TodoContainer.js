e2e843b4d2b545fd79adb8dd0f77cd7aeb8cddbb
Will Forbes <wforbes87@gmail.com>
2022-03-13 11:44:15 -0700

    Add TodoContainer component, import it in index js

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class TodoContainer extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello, universe.</h1>
+				<p>I am a React component!</p>
+			</div>
+		)
+	}
+}
+export default TodoContainer
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const element = <h1>Hello universe</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+import TodoContainer from "./components/TodoContainer"
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
