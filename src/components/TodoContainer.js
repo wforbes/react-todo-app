d0a701c378e3fd81ebb8e9065886787af35291d3
Will Forbes <wforbes87@gmail.com>
2022-03-13 12:14:48 -0700

    Add state to TodoContainer

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,37 @@
 import React from "react"
 
 class TodoContainer extends React.Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: "Do the laundry",
+				completed: true
+			},
+			{
+				id: 2,
+				title: "Clean the office",
+				completed: false
+			},
+			{
+				id: 3,
+				title: "Get game inventory ready to sort",
+				completed: false
+			},
+			{
+				id: 4,
+				title: "Clean out the car",
+				completed: false
+			}
+		]
+	}
 	render() {
 		return (
-			<div>
-				<h1>Hello, universe.</h1>
-				<p>I am a React component!</p>
-			</div>
+			<ul>
+				{this.state.todos.map(todo => (
+					<li>{todo.title}</li>
+				))}
+			</ul>
 		)
 	}
 }
